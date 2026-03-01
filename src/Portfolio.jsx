d5d2e23ed4444296bdd5eb629bc28815d9207732
Simran Manturgimath <simran.rm6@gmail.com>
2026-03-01 17:18:14 +0530

    Refactor Portfolio component layout and styles
    Adjusted padding and text size for better layout.

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -20,11 +20,11 @@ const Portfolio = ({ about, image }) => {
           {/* Text Section */}
           <div className="md:w-1/2 flex flex-col items-center md:items-center text-center md:pl-4 max-w-md">
             <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-4 mt-14 sm:mt-0">Hello👋🏻</h2>
-            <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-2">I'm Simran</h3>
-            <p className="text-gray-600 text-sm sm:text-base md:text-lg mb-6">{about}</p>
+            <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-2 pt-2">I'm Simran</h3>
+            <p className="text-gray-600 text-sm sm:text-base md:text-sm pt-4 mb-6">{about}</p>
 
             {/* Links Section */}
-            <div className="flex flex-wrap justify-center gap-6">
+            <div className="flex flex-wrap justify-center gap-6 pt-4">
               <Link 
                 to="/resume" 
                 className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 flex items-center justify-center bg-yellow-500 text-white rounded-full text-xs sm:text-sm md:text-lg font-semibold hover:bg-yellow-600 transition-colors"
